afa0d91835824f7259214c5bc0eebedbbbeab8f9
luchoooo13 <studiostintf@gmail.com>
2026-08-04 21:26:30 -0300

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
 const config = getDefaultConfig(__dirname);
+
+// react-native-css-interop escribe un archivo de cache interno
+// (.cache/web.css) durante el bundling cuando forceWriteFileSystem
+// esta activado. En un build "limpio" (Docker/CI, sin watchman previo)
+// ese archivo se crea a mitad del proceso y Metro no lo encuentra al
+// intentar calcular su SHA-1, lo que tira:
+// "Failed to get the SHA-1 for: .../react-native-css-interop/.cache/web.css"
+// Lo excluimos del resolver porque es solo un cache interno, no algo
+// que necesitemos bundlear.
+config.resolver.blockList = exclusionList([
+  /node_modules\/react-native-css-interop\/\.cache\/.*/,
+]);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
